b7216bfe3a51ea8be8c6a59dd4101cca9556a34d
Samuel Burkinshaw <sambkw123@gmail.com>
2025-08-15 18:35:49 +0100

    sveltekit initialisation

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel='icon' href={favicon} />
+</svelte:head>
+
+<h1>S-Burks</h1>
+
+<nav>
+	<a href='/'>Home</a>
+	<a href='/about'>About</a>
+	<a href='/film'>Film</a>
+	<a href='/programming'>Programming</a>
+</nav>
+
+{@render children?.()}
