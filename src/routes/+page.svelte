75715f36c60886e495fe5486ec4ba5e657e05f25
Samuel Burkinshaw <sambkw123@gmail.com>
2025-08-15 23:05:51 +0100

    font attempts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h2>welcome to my website</h2>
+<p>it is currently under construction</p>
